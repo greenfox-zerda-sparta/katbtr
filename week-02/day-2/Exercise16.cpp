f672645ac8dcbf4fc72a701ee0d3e4303edf2e40
katbtr <bisztrayk@gmail.com>
2016-10-25 17:12:43 +0200

    Added Exercise 16ab

---
+++ week-02/day-2/Exercise16.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void MatrixRow(int matri[3][3]) {
+
+  for (int i = 0; i < 3; i++) {
+
+    cout << endl;
+    for (int j = 0; j < 3; j++) {
+
+      cout << matri[i][j] << " ";
+    }
+  }
+}
+
+void MatrixColumn(int matri[3][3]) {
+
+  for (int j = 0; j < 3; j++) {
+
+    cout << endl;
+    for (int i = 0; i < 3; i++) {
+
+      cout << matri[i][j] << " ";
+    }
+  }
+}
+
+int main(){
+  int m[3][3]= {{1,2,3},{4,5,6},{7,8,9}};
+  //This is an array of arrays. Print it out as a 3x3 matrix. Using each inner array as
+  //a.) a row
+  //b.) a column
+  //c.) all in one line separated by the character '|'
+  //d.) all in one column
+
+  MatrixRow(m);
+  cout << endl;
+  MatrixColumn(m);
+  cout << endl;
+
+  return 0;
+}
