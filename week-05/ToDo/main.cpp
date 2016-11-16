0e2fa3c8ba4857c86f0088cf949a0e3f88d48100
katbtr <bisztrayk@gmail.com>
2016-11-16 15:52:34 +0100

    Todo: (feat) -l working

--- week-05/ToDo/main.cpp
+++ week-05/ToDo/main.cpp
@@ -24,13 +24,28 @@ public:
 };
 */
 
+void list_tasks(string _filename) {
+    ifstream my_file;
+    my_file.open(_filename);
+    if (my_file.is_open()) {
+        string file_content;
+        unsigned int line_number = 1;
+        while (my_file >> file_content) {
+            cout << line_number << " - " << file_content << endl;
+            line_number++;
+        }
+    } else {
+        cerr << "Could not open the file";
+    }
+    my_file.close();
+}
 
 int main(int argc, char** argv) {
 
     if (argc == 1) {
         print_usage();
     } else if (string(argv[1]) == "-l") {
-            cout << "Bassza meg működik!!!\n";
+            list_tasks("todolist.txt");
         } else if (string(argv[1]) == "-a") {
             cout << "Most -a -val műxik \n";
         } else if (string(argv[1]) == "-r") {
@@ -42,10 +57,10 @@ int main(int argc, char** argv) {
         }
 
 
-/*
+
     make_file("todolist.txt");
 
-    */
+
 
 
     return 0;
