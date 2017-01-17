f2dd8a560c8abd8e6c7ace98974d3c5bd88201fe
katbtr <bisztrayk@gmail.com>
2017-01-17 15:16:31 +0100

    Csharp practice: Diamond

---
+++ csharp practice/Diamond/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void DrawInvertedPyramid(int n)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me how many rows the pyramid should have");
+            string x = Console.ReadLine();
+            int row = Convert.ToInt32(x);
+            DrawPyramid(row);
+            DrawInvertedPyramid(row);
+            Console.ReadLine();
+        }
+    }
+}
