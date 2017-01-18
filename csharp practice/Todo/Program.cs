5bf4aae0128378911fa3150bebe09aa50ca5ac97
katbtr <bisztrayk@gmail.com>
2017-01-18 11:58:27 +0100

    Csharp practice: Todo - filehandling, addtask

---
+++ csharp practice/Todo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoCS
+{
+    class Program
+    {
+        //void print_usage()
+        //{
+        //    Console.WriteLine("CLI Todo application");
+        //    Console.WriteLine("====================");
+        //    Console.WriteLine();
+        //    Console.WriteLine("Command line arguments:");
+        //    Console.WriteLine("-l   Lists all the tasks");
+        //    Console.WriteLine("-a   Adds a new task");
+        //    cout << "-r   Removes a task" << endl;
+        //    cout << "-c   Completes a task" << endl << endl;
+        //}
+
+        static void Main(string[] args)
+        {
+            //string argument;
+            //argument = Console.ReadLine();
+
+            //switch (argument)
+            //{
+            //  case "-l":
+            //    Console.WriteLine("This should be help.");
+            //    break;
+
+            //   case "-a":
+            //     Console.WriteLine("This should be version.");
+            //    break;
+
+            //    case "-c":
+            //      quitNow = true;
+            //    break;
+
+            //    default:
+            //      Console.WriteLine("Unknown Command " + command);
+            //    break;
+            //}
+
+            TaskHandler taskhandler = new TaskHandler();
+            taskhandler.PrintList();
+            Console.WriteLine();
+            Console.WriteLine("Type the task to add: ");
+            string toAdd = Console.ReadLine();
+            taskhandler.AddTask(toAdd);
+            Console.ReadLine();
+
+        }       
+    }
+}
---
+++ csharp practice/Todo/TaskHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TodoCS
+{
+    public class TaskHandler
+    {
+        private string filepath = "d:\\winde/visual studio 2015/Projects/TodoCS/TodoCS/todolist.txt";
+        public string Filepath { get; set; }
+
+        private List<string> todolist = new List<string>();
+
+        public void ReadFromFile()
+        {
+            int counter = 0;
+            string line;
+            StreamReader file = new StreamReader(filepath);
+            while ((line = file.ReadLine()) != null)
+            {
+                string task = "[ ] " + (counter + 1).ToString() + " - " + line;
+                todolist.Add(task);
+
+                counter++;
+            }
+
+            file.Close();
+        }
+
+        public void PrintList()
+        {
+            ReadFromFile();
+            foreach (string task in todolist)
+            {
+                Console.WriteLine(task);
+            }
+    
+        }
+
+        public void AddTask(string tasktoadd)
+        {
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine(tasktoadd);
+            }
+        }
+    }
+}
